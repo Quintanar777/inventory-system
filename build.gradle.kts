a90fd995f181a6d6c6955480e5ac5fe4ec66effd
Erick Quintanar <quintanar777@gmail.com>
2025-09-04 20:17:50 -0600

    feat: remove plugin error

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
     id("com.vaadin") version "24.8.6"
     kotlin("plugin.jpa") version "1.9.25"
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 group = "com.perroamor"
